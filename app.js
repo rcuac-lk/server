77dce4cefa146c5e99999bfe6866f3f354cab44e
Kushantha <kushantha018@gmail.com>
2025-02-18 21:53:55 +0530

    Add new routes

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ app.get("/", (req, res) => {
 // routes
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
+require("./app/routes/coach.routes")(app);
+require("./app/routes/manager.routes")(app);
+require("./app/routes/parent.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
