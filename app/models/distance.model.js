38a74c496638d051dbc56ab74b667c325f06247f
Kushantha <kushantha018@gmail.com>
2025-07-01 23:08:30 +0530

    Update distance and session models

--- app/models/distance.model.js
+++ app/models/distance.model.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, Sequelize) => {
           type: Sequelize.INTEGER,
           allowNull: false,
         },
+        Active: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: true,
+        },
       },
       {
         freezeTableName: true,
